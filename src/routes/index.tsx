39f84c9d08a36fe2cd5278a549fd768e320358d2
Lee <43488305+poiu694@users.noreply.github.com>
2022-10-03 14:06:47 +0900

    feat: ModalTestPage 만들기

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-import { CarouselPage, LoginPage } from '../pages';
+import { CarouselPage, LoginPage, ModalTestPage } from '../pages';
 
 function Router() {
   return (
     <Routes>
       <Route path='/' element={<LoginPage />} />
       <Route path='/carousel' element={<CarouselPage />} />
+      <Route path='/modal' element={<ModalTestPage />} />
     </Routes>
   );
 }
