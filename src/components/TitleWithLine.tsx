52942394ee23a0db748407f0c247e371d5e5f68e
Lee <43488305+poiu694@users.noreply.github.com>
2022-10-04 10:33:58 +0900

    feat: 사이드에 아이콘 있는 TitleWithLine 타입 만들기

--- src/components/TitleWithLine.tsx
+++ src/components/TitleWithLine.tsx
@@ -2,11 +2,14 @@ import styled from '@emotion/styled';
 import { ComponentPropsWithoutRef, CSSProperties } from 'react';
 import { PalleteValueType } from '../styles/theme/colors';
 import { FontKeyType } from '../styles/theme/fonts';
-import { Typography } from './common';
+import { Icon, Typography } from './common';
+
+type Type = 'normal' | 'icon';
 
 interface Props extends ComponentPropsWithoutRef<'div'> {
   title: string;
   titleFontType: FontKeyType;
+  type?: Type;
   titleColor?: PalleteValueType;
   borderColor?: PalleteValueType;
   textAlign?: CSSProperties['textAlign'];
@@ -15,25 +18,29 @@ interface Props extends ComponentPropsWithoutRef<'div'> {
 function TitleWithLine({
   title,
   titleFontType,
+  type = 'normal',
   titleColor,
   textAlign,
   borderColor,
   ...restProps
 }: Props) {
   return (
-    <Wrapper borderColor={borderColor} textAlign={textAlign} {...restProps}>
+    <Wrapper type={type} borderColor={borderColor} textAlign={textAlign} {...restProps}>
       <Typography type={titleFontType} color={titleColor} textAlign={textAlign}>
         {title}
       </Typography>
+      {type === 'icon' && <Icon type='mic' style={{ cursor: 'pointer' }} />}
     </Wrapper>
   );
 }
 
-type StyleProps = Pick<Props, 'textAlign' | 'borderColor'>;
+type StyleProps = Pick<Props, 'textAlign' | 'borderColor' | 'type'>;
 
 const Wrapper = styled.div<StyleProps>`
+  display: ${({ type }) => (type === 'icon' ? 'flex' : 'block')};
+  justify-content: ${({ type }) => type === 'icon' && 'space-between'};
+  align-items: ${({ type }) => type === 'icon' && 'center'};
   text-align: ${({ textAlign }) => textAlign};
-
   border-bottom: 2px solid ${({ borderColor }) => borderColor};
 `;
 
