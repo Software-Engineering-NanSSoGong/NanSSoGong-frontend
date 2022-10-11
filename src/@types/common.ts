be0a06641045ab7c9cc242936a40798f86486089
Lee <43488305+poiu694@users.noreply.github.com>
2022-10-11 14:37:43 +0900

    feat: mr-daebak 기본 인터페이스 설정

---
+++ src/@types/common.ts
@@ -0,0 +1,19 @@
+export interface BaseFood<T> {
+  name: T;
+  price: number;
+  quantity?: number;
+}
+
+export type StyleName = 'simple' | 'deluxe' | 'grande';
+
+export interface Style extends BaseFood<StyleName> {}
+
+export interface Dinner extends BaseFood<string> {
+  image: string;
+  description: string;
+  styles: Style[];
+}
+
+export interface Food extends BaseFood<string> {
+  type: 'meat' | 'rice' | 'drink';
+}
