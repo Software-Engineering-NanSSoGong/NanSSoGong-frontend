f64eb60078d6cf6e1dd2f6accdd53ad9dea8d630
Lee <43488305+poiu694@users.noreply.github.com>
2022-10-15 00:47:12 +0900

    fix: type 잘못 내려준 오타 수정

--- src/pages/ItemDetailPage.tsx
+++ src/pages/ItemDetailPage.tsx
@@ -24,7 +24,7 @@ function ItemDetailPage() {
       <SideMenuList />
       <Spacer>
         <FoodBox
-          type='order'
+          type='beforeOrder'
           dinner={dinner}
           setDinner={setDinner}
           selectedStyle={selectedStyle}
