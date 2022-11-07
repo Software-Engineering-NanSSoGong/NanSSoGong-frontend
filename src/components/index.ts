c8868c7781f9eb397a438c7da10d31c6e5aec67a
Lee <43488305+poiu694@users.noreply.github.com>
2022-11-08 01:14:57 +0900

    feat: 주문내역 확인 모달컴포넌트 제작

--- src/components/index.ts
+++ src/components/index.ts
@@ -11,3 +11,4 @@ export { default as FoodQuantityBoxList } from './FoodQuantityBoxList';
 export { default as BottomButton } from './BottomButton';
 export { default as FoodBox } from './FoodBox';
 export { default as ClickableIcon } from './ClickableIcon';
+export { default as OrderConfirmBox } from './OrderConfirmBox';
