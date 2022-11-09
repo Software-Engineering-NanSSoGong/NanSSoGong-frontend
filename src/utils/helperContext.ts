74867ee68a31861c77e53cc91efd001e512c8dc2
Lee <43488305+poiu694@users.noreply.github.com>
2022-11-09 18:51:27 +0900

    style: default import로 통일

--- src/utils/helperContext.ts
+++ src/utils/helperContext.ts
@@ -1,12 +1,12 @@
-import React, { createContext, useContext } from 'react';
+import React from 'react';
 
 export function createGenericContext<T>() {
-  return createContext<T>({} as T);
+  return React.createContext<T>({} as T);
 }
 
 export function createGenericUseFunction<T>(context: React.Context<T>): () => NonNullable<T> {
   return (): NonNullable<T> => {
-    const value: T = useContext<T>(context as React.Context<T>);
+    const value: T = React.useContext<T>(context as React.Context<T>);
 
     return value as NonNullable<T>;
   };
