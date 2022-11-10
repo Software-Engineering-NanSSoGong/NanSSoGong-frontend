58476b13fc703d57854ad14e21f726cf608508a1
Lee <43488305+poiu694@users.noreply.github.com>
2022-11-10 16:30:17 +0900

    feat: OrderService 제작

---
+++ src/api/OrderService.ts
@@ -0,0 +1,37 @@
+import {
+  BasePageRequest,
+  BaseRequsetId,
+  History,
+  ResponseFoodList,
+  ResponseOrderHistoryList,
+} from '../@types';
+import APIBase from './core';
+
+class OrderService extends APIBase {
+  public constructor() {
+    super('order');
+  }
+
+  public getClientHistory({ page, size }: BasePageRequest): Promise<ResponseOrderHistoryList> {
+    return this.baseHTTP
+      .get(`client/list?page=${page}size=${size}`)
+      .then(APIBase._handleResponse)
+      .catch(APIBase._handleError);
+  }
+
+  public getGuestHistory({ id }: BaseRequsetId): Promise<History> {
+    return this.baseHTTP
+      .get(`guest/${id}`)
+      .then(APIBase._handleResponse)
+      .catch(APIBase._handleError);
+  }
+
+  public getList({ page, size }: BasePageRequest): Promise<ResponseFoodList> {
+    return this.baseHTTP
+      .get(`list?page=${page}&size=${size}`)
+      .then(APIBase._handleResponse)
+      .catch(APIBase._handleError);
+  }
+}
+
+export default new OrderService();
