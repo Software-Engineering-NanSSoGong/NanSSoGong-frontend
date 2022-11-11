ade391a8b044eb2e6f5f720bcdb2043fb715498a
Lee <43488305+poiu694@users.noreply.github.com>
2022-11-11 12:54:38 +0900

    fix: 장바구니 페이지에서 데이터가 없는 경우 핸들링

--- src/pages/OrderPage.tsx
+++ src/pages/OrderPage.tsx
@@ -34,100 +34,113 @@ function OrderPage() {
   return (
     <Wrapper>
       <SideMenuList />
-      <Spacer>
-        <TitleWithLine title='주문하기' titleFontType='h1' />
-        {myBagState?.map((item, idx) => (
-          <FoodBox
-            key={idx}
-            type='order'
-            dinner={item.dinner}
-            selectedStyle={item.selectedStyle}
-            addedFoodInfos={item.addedFoodInfos}
-            reducedFoodInfos={item.reducedFoodInfos}
+      {myBagState.length !== 0 ? (
+        <>
+          <Spacer>
+            <TitleWithLine title='주문하기' titleFontType='h1' />
+            {myBagState?.map((item, idx) => (
+              <FoodBox
+                key={idx}
+                type='order'
+                dinner={item.dinner}
+                selectedStyle={item.selectedStyle}
+                addedFoodInfos={item.addedFoodInfos}
+                reducedFoodInfos={item.reducedFoodInfos}
+              />
+            ))}
+            <OrderInfomationBox>
+              <Typography type='h3' color={theme.colors.text.bold}>
+                배달 정보
+              </Typography>
+              <LabelWithInput>
+                <Typography type='h4' color={theme.colors.text.bold}>
+                  주문자 이름
+                </Typography>
+                <Inputs>
+                  <Input style={{ width: '20%' }} />
+                </Inputs>
+              </LabelWithInput>
+              <LabelWithInput>
+                <Typography type='h4' color={theme.colors.text.bold}>
+                  상세 주소
+                </Typography>
+                <Inputs>
+                  <Input />
+                  <Input />
+                  <Input />
+                </Inputs>
+              </LabelWithInput>
+            </OrderInfomationBox>
+            <OrderInfomationBox>
+              <Typography type='h3' color={theme.colors.text.bold}>
+                결제 수단
+              </Typography>
+              <LabelWithInput>
+                <Typography type='h4' color={theme.colors.text.bold}>
+                  카드 회사
+                </Typography>
+                <Inputs>
+                  <Input style={{ width: '20%' }} />
+                </Inputs>
+              </LabelWithInput>
+              <LabelWithInput>
+                <Typography type='h4' color={theme.colors.text.bold}>
+                  카드 번호
+                </Typography>
+                <Inputs>
+                  <Input />
+                  <Input />
+                  <Input />
+                  <Input />
+                </Inputs>
+              </LabelWithInput>
+            </OrderInfomationBox>
+            <OrderInfomationBox>
+              <PriceBox totalPrice={totalPrice} />
+            </OrderInfomationBox>
+          </Spacer>
+          <Modal
+            triggerNode={
+              <Modal.triggerButton
+                modalType='open'
+                onClick={handleClickModalConfirmButton}
+                style={{
+                  position: 'fixed',
+                  marginLeft: '300px',
+                  width: 'calc(100% - 300px)',
+                }}
+                buttonProps={{
+                  hierarchy: ButtonHierarchy.Danger,
+                  disabled: myBagState.length === 0,
+                }}
+                as={BottomButton}
+              >
+                <Typography type='h3' textAlign='center'>
+                  주문하기
+                </Typography>
+              </Modal.triggerButton>
+            }
+            modalNode={
+              <Modal.askModal>
+                <ModalBody>
+                  <Typography type='h3' color={theme.colors.text.dark} textAlign='center'>
+                    구매 확정
+                  </Typography>
+                  <Typography type='body4' color={theme.colors.text.dark} style={{ marginTop: 32 }}>
+                    총 {totalPrice.toLocaleString()} 원입니다.정말로 구매하시겠습니까?
+                  </Typography>
+                </ModalBody>
+              </Modal.askModal>
+            }
           />
-        ))}
-        <OrderInfomationBox>
-          <Typography type='h3' color={theme.colors.text.bold}>
-            배달 정보
+        </>
+      ) : (
+        <Spacer>
+          <Typography type='h3' color={theme.colors.primary.red} textAlign='center'>
+            장바구니에 디너를 담아 주세요.
           </Typography>
-          <LabelWithInput>
-            <Typography type='h4' color={theme.colors.text.bold}>
-              주문자 이름
-            </Typography>
-            <Inputs>
-              <Input style={{ width: '20%' }} />
-            </Inputs>
-          </LabelWithInput>
-          <LabelWithInput>
-            <Typography type='h4' color={theme.colors.text.bold}>
-              상세 주소
-            </Typography>
-            <Inputs>
-              <Input />
-              <Input />
-              <Input />
-            </Inputs>
-          </LabelWithInput>
-        </OrderInfomationBox>
-        <OrderInfomationBox>
-          <Typography type='h3' color={theme.colors.text.bold}>
-            결제 수단
-          </Typography>
-          <LabelWithInput>
-            <Typography type='h4' color={theme.colors.text.bold}>
-              카드 회사
-            </Typography>
-            <Inputs>
-              <Input style={{ width: '20%' }} />
-            </Inputs>
-          </LabelWithInput>
-          <LabelWithInput>
-            <Typography type='h4' color={theme.colors.text.bold}>
-              카드 번호
-            </Typography>
-            <Inputs>
-              <Input />
-              <Input />
-              <Input />
-              <Input />
-            </Inputs>
-          </LabelWithInput>
-        </OrderInfomationBox>
-        <OrderInfomationBox>
-          <PriceBox totalPrice={totalPrice} />
-        </OrderInfomationBox>
-      </Spacer>
-      <Modal
-        triggerNode={
-          <Modal.triggerButton
-            modalType='open'
-            onClick={handleClickModalConfirmButton}
-            style={{
-              position: 'fixed',
-              marginLeft: '300px',
-              width: 'calc(100% - 300px)',
-            }}
-            buttonProps={{ hierarchy: ButtonHierarchy.Danger, disabled: myBagState.length === 0 }}
-            as={BottomButton}
-          >
-            <Typography type='h3' textAlign='center'>
-              주문하기
-            </Typography>
-          </Modal.triggerButton>
-        }
-        modalNode={
-          <Modal.askModal>
-            <ModalBody>
-              <Typography type='h3' color={theme.colors.text.dark} textAlign='center'>
-                구매 확정
-              </Typography>
-              <Typography type='body4' color={theme.colors.text.dark} style={{ marginTop: 32 }}>
-                총 {totalPrice.toLocaleString()} 원입니다.정말로 구매하시겠습니까?
-              </Typography>
-            </ModalBody>
-          </Modal.askModal>
-        }
-      />
+        </Spacer>
+      )}
     </Wrapper>
   );
 }
