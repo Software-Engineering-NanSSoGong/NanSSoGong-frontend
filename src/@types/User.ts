d7cfb5e4398a994085d80849192dc3bc1bb08201
Lee <43488305+poiu694@users.noreply.github.com>
2022-11-12 17:24:06 +0900

    feat: User Grade 정의

--- src/@types/User.ts
+++ src/@types/User.ts
@@ -1,1 +1,27 @@
 export type UserType = 'client' | 'employee' | 'rider';
+
+export const GRADE_INFO = {
+  BRONZE: {
+    clientGradeName: 'BRONZE',
+    cut: 0,
+    saleRate: 0,
+  },
+  GOLD: {
+    clientGradeName: 'GOLD',
+    cut: 1,
+    saleRate: 3,
+  },
+  DIAMOND: {
+    clientGradeName: 'DIAMOND',
+    cut: 3,
+    saleRate: 5,
+  },
+  CHALLENGER: {
+    clientGradeName: 'CHALLENGER',
+    cut: 5,
+    saleRate: 10,
+  },
+} as const;
+
+export type GRADE = keyof typeof GRADE_INFO;
+
